dc138c464160b638c1b7bcbcf8fa4b7c63a42e6d
arrayreduction <yblad@hotmail.co.uk>
2023-04-12 16:50:00 +0100

    - Added imports for covid 19 deaths by age and gender - Added nrows argument to import_excel_sheets - Extended support for tuples in import_excel_sheets nrows and skiprows - Fixed a bug where temporary files were not removed from the file   list, causing issues if they were open in excel and being imported

--- Analysis.py
+++ Analysis.py
@@ -3,11 +3,36 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 
-def import_excel_sheets(sheet_no, skiprows, files):
+def import_excel_sheets(sheet_no, skiprows, files, nrows=None):
+    '''Import data from multiple like-formated excel sheets.
+    sheet_no: 0 indexed sheet number to lookup from excel workbooks
+    skiprows: either an integer or tuple indicating which header rows should be skipped 
+    nrows: either an integer or a tuple giving the number of rows to read in each sheet
+    '''
     dfs = []
-    for f in files:
-        df = pd.read_excel(f, sheet_name=sheet_no, skiprows=skiprows)
-        dfs.append(df)
+
+    #In the simple case, just interate through the files and append them to an array
+    #Where there are tuples, we unpack accordingly
+
+    if (nrows is None and type(skiprows) is int) or (type(nrows) is int and type(skiprows is int)):
+        for f in files:
+            df = pd.read_excel(f, sheet_name=sheet_no, skiprows=skiprows, nrows=nrows)
+            dfs.append(df)
+
+    else:
+        for i, f in enumerate(files):
+            if type(nrows) is tuple:
+                nrow = nrows[i]
+            else:
+                nrow = nrows
+            
+            if type(skiprows) is tuple:
+                skiprow = skiprows[i]
+            else:
+                skiprow = skiprows
+
+            df = pd.read_excel(f, sheet_name=sheet_no, skiprows=skiprow, nrows=nrow)
+            dfs.append(df)
 
     return dfs
 
@@ -18,7 +43,10 @@ def get_files(dir, src):
     sub_src = os.path.join(src, dir)
 
     #Get all filenames with filepath
-    files = [os.path.join(dirpath,f) for (dirpath, dirnames, filenames) in os.walk(sub_src) for f in filenames]
+    files = [os.path.join(dirpath, f) for (dirpath, dirnames, filenames) in os.walk(sub_src) for f in filenames]
+
+    #For saftey, strip any temp files in windows environments
+    files = [x for x in files if "~$" not in x]
 
     return files
 
@@ -48,6 +76,21 @@ def main():
     df_vacs = pd.concat(dfs, axis=0, sort=False)
 
     df_vacs.info()
+
+    #Handle covid deaths by age by gender
+    
+    #All by age
+    df_cv19_age_all = pd.concat(import_excel_sheets(7, 6, files_deaths_post22, nrows=(64, 104)))
+
+    #Male by age
+    df_cv19_age_male = pd.concat(import_excel_sheets(7, (73, 113), files_deaths_post22, nrows=(64, 104)))
+
+    #Female by age
+    df_cv19_age_female = pd.concat(import_excel_sheets(7, (140, 220), files_deaths_post22, nrows=(64, 104)))
+
+    df_cv19_age_all.info()
+    df_cv19_age_male.info()
+    df_cv19_age_female.info()
 
 
 if __name__ == '__main__':
